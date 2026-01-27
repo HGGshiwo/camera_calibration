fae9586edfeb14f9f919cbc9b44d6e92a43a5418
HGGshiwo <3190102788@zju.edu.cn>
2026-01-27 17:18:34 +0800

    update build

---
+++ sh/build.sh
@@ -0,0 +1,4 @@
+nuitka --standalone --onefile --include-data-dir=app/dist=app/dist app.py --output-dir build/windows-x86
+nuitka --standalone --onefile --include-data-dir=app/dist=app/dist app.py --output-dir build/linux-arm
+
+
